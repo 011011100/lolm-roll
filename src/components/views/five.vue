4292572227bca3c3249851fea351107f0dc08431
LZH <LZH01101110@163.com>
2024-03-04 21:01:21 +0800

    添加 随机展示页面

---
+++ src/components/views/five.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+import Case from "@/components/views/case.vue";
+import {computed} from "vue";
+
+const props = defineProps({
+  rollDataList: Array
+})
+
+const sort = computed(() => {
+  let showDataList = []
+  for (let rollDataListElement of props.rollDataList) {
+    switch (rollDataListElement.positionRoll) {
+      case "上单":
+        showDataList[0] = rollDataListElement
+        break
+      case "打野":
+        showDataList[1] = rollDataListElement
+        break
+      case "中单":
+        showDataList[2] = rollDataListElement
+        break
+      case "射手":
+        showDataList[3] = rollDataListElement
+        break
+      case "辅助":
+        showDataList[4] = rollDataListElement
+        break
+    }
+  }
+  return showDataList
+})
+
+</script>
+
+<template>
+  <div v-for="index of sort" style="display: flex;
+    padding-left: 8px;
+    padding-right: 8px;
+    flex-direction: column;">
+    <case :roll-data="index"></case>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/views/one.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Case from "@/components/views/case.vue";
+
+const props = defineProps({
+  rollData: Object
+})
+
+</script>
+
+<template>
+  <div class="one-case">
+    <case :roll-data="rollData"></case>
+  </div>
+</template>
+
+<style>
+
+.one-case {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
