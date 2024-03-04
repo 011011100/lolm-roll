76f03160ded953a21cfc07638b1d3f33fb460fa7
LZH <LZH01101110@163.com>
2024-03-04 21:01:59 +0800

    添加 设置图片大小跟路径

--- src/components/views/character/character.vue
+++ src/components/views/character/character.vue
@@ -2,15 +2,23 @@
   import {computed} from "vue";
   const props = defineProps({
     characterName: String,
+    weight: {
+      type: Number,
+      default: 308
+    },
+    height: {
+      type: Number,
+      default: 560
+    }
   })
 
   const ImgUrl = computed(()=>{
-    return "/assets/characterImg/" + props.characterName + ".jpg"
+    return "/assets/character/" + props.characterName + ".jpg"
   })
 </script>
 
 <template>
-  <img :src="ImgUrl" :alt="characterName">
+  <img :src="ImgUrl" :alt="characterName" :width=weight :height=height>
 </template>
 
 <style >
