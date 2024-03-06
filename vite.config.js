2715c66059fff994d1d55a8a1ed699150868ea59
elonehoo <hi@elonehoo.me>
2024-03-06 09:38:08 +0800

    fix: modify the file name

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ import { DestylerImageResolver } from '@destyler/image/resolver'
 export default defineConfig({
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   plugins: [
